c9ab2bc64ce4091b2c2b976c0a51e1cae3bbf020
unknown <temonbocil40@gmail.com>
2026-07-09 08:22:34 +0700

    set up awal

---
+++ database/migrations/2026_07_09_010153_create_jadwals_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+                Schema::create('jadwals', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('dosen_id')->constrained('dosens')->cascadeOnDelete();
+
+            $table->foreignId('mata_kuliah_id')->constrained('mata_kuliahs')->cascadeOnDelete();
+
+            $table->foreignId('kelas_id')->constrained('kelas')->cascadeOnDelete();
+
+            $table->enum('hari', [
+                'Senin',
+                'Selasa',
+                'Rabu',
+                'Kamis',
+                'Jumat',
+                'Sabtu'
+            ]);
+
+            $table->time('jam_mulai');
+            $table->time('jam_selesai');
+
+            $table->timestamps();
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jadwals');
+    }
+};
